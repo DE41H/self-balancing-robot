0a9d61d9bc33dbe35d2c9daec07ef4aaa4c5d2c5
sreyash <sreyash.s@outlook.com>
2025-11-16 14:28:16 +0530

    improved pid_controller performance

--- firmware/lib/PID_CONTROLLER/pid_controller.cpp
+++ firmware/lib/PID_CONTROLLER/pid_controller.cpp
@@ -1,65 +1,24 @@
 #include <pid_controller.hpp>
 
-static const TickType_t xLoopPeriod = (1000 / PIDController::SAMPLE_FREQ_HZ) / portTICK_PERIOD_MS;
-
-PIDController::PIDController(const double kp, const double ki, const double kd, const double min, const double max, const String name, const byte priority, QueueHandle_t inputQueue):
+PIDController::PIDController(const double kp, const double ki, const double kd, const double min, const double max, QueueHandle_t inputQueue):
 _input(0),
 _output(0),
 _setpoint(0),
 _pid(&_input, &_output, &_setpoint, kp, ki, kd, DIRECT),
-_name(name),
-_priority(priority),
 _inputQueue(inputQueue)
 {
     init(min, max);
 }
 
 void PIDController::init(const double min, const double max) {
-    _outputQueue = xQueueCreate(1, sizeof(double));
-    if (_outputQueue == NULL) {
-        Serial.println("Failed to create yaw queue!");
-        return;
-    }
-
-    BaseType_t taskCreated = xTaskCreatePinnedToCore(
-        taskTrampoline,
-        "Gyro Task",
-        TASK_STACK_SIZE,
-        this,
-        _priority,
-        &_taskHandle,
-        TASK_CORE_ID
-    );
-    if (taskCreated != pdPASS) {
-        Serial.println("Failed to create " + _name + " task!");
-        vQueueDelete(_outputQueue);
-        return;
-    }
-
     _pid.SetSampleTime(SAMPLE_TIME);
     _pid.SetOutputLimits(min, max);
     _pid.SetMode(AUTOMATIC);
 }
 
-void PIDController::update() {
+double PIDController::compute() {
     double buffer;
     _input = xQueuePeek(_inputQueue, &buffer, NULL);
     _pid.Compute();
-    xQueueOverwrite(_outputQueue, &_output);
-}
-
-void PIDController::taskLoop() {
-    TickType_t xLastWakeTime = xTaskGetTickCount();
-    while (true) {
-        update();
-        xTaskDelayUntil(&xLastWakeTime, xLoopPeriod);
-    }
-}
-
-void PIDController::taskTrampoline(void* pvParameters) {
-    PIDController* instance = static_cast<PIDController*>(pvParameters);
-    
-    instance->taskLoop();
-
-    vTaskDelete(NULL);
+    return _output;
 }
